b3c9854c92a560a631ea816f0038cbf892214701
Daniel Mendes <danielmendes.sw@gmail.com>
2023-09-30 22:55:22 +0200

    Feat: add jpa repositories

---
+++ src/main/java/com/challenge/challenge/repository/ConsultRepository.java
@@ -0,0 +1,7 @@
+package com.challenge.challenge.repository;
+
+import com.challenge.challenge.model.Consult;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ConsultRepository extends JpaRepository<Consult, Long> {
+}
---
+++ src/main/java/com/challenge/challenge/repository/SymptomRepository.java
@@ -0,0 +1,7 @@
+package com.challenge.challenge.repository;
+
+import com.challenge.challenge.model.Consult;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SymptomRepository extends JpaRepository<Consult, Long> {
+}
