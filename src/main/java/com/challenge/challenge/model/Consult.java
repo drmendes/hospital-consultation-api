e5aea992a7ed348d5d97e4bade249b0e71a26e9e
Daniel Mendes <danielmendes.sw@gmail.com>
2023-10-02 19:42:15 +0200

    Feat: add getters/setters

--- src/main/java/com/challenge/challenge/model/Consult.java
+++ src/main/java/com/challenge/challenge/model/Consult.java
@@ -13,20 +13,44 @@ public class Consult {
     private Long id;
 
     @ManyToOne
-    @JoinColumn(name = "doctor_id")
+    @JoinColumn(name = "doctor_id", nullable = false)
     private Doctor doctor;
 
     @ManyToOne
-    @JoinColumn(name = "speciality_id")
+    @JoinColumn(name = "speciality_id", nullable = false)
     private Speciality speciality;
 
     @ManyToOne
-    @JoinColumn(name = "patient_id")
+    @JoinColumn(name = "patient_id", nullable = false)
     private Patient patient;
 
     @ManyToOne
     @JoinColumn(name = "pathology_id")
     private Pathology pathology;
 
-    // TODO Getter, Setter
+
+    public Speciality getSpeciality() {
+        return this.speciality;
+    }
+
+    public Doctor getDoctor() {
+        return this.doctor;
+    }
+
+    public void setDoctor(Doctor doctor) {
+        this.doctor = doctor;
+    }
+
+    public void setSpeciality(Speciality speciality) {
+        this.speciality = speciality;
+    }
+
+    public void setPatient(Patient patient) {
+        this.patient = patient;
+    }
+
+    public void setPathology(Pathology pathology) {
+        this.pathology = pathology;
+    }
 }
+
