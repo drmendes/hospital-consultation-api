759c658f76fa537cfff887970db6e16639133f5b
Daniel Mendes <danielmendes.sw@gmail.com>
2023-10-02 19:45:51 +0200

    Feat: add exception handler for not found

---
+++ src/main/java/com/challenge/challenge/controller/GlobalExceptionHandler.java
@@ -0,0 +1,26 @@
+package com.challenge.challenge.controller;
+
+
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Map<String, String> handleEntityNotFoundException(EntityNotFoundException ex) {
+        Map<String, String> response = new HashMap<>();
+        response.put("error", "Bad Request");
+        response.put("message", ex.getMessage());
+        return response;
+    }
+
+}
+
