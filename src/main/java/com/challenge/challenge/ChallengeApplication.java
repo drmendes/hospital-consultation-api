37c5e9b3de2f39094e56a87d038262e809adbbe7
Daniel Mendes <danielmendes.sw@gmail.com>
2023-10-02 19:32:36 +0200

    Fix: add paths to spring

--- src/main/java/com/challenge/challenge/ChallengeApplication.java
+++ src/main/java/com/challenge/challenge/ChallengeApplication.java
@@ -3,7 +3,7 @@ package com.challenge.challenge;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.challenge.challenge.controller","com.challenge.challenge.repository", "com.challenge.challenge.service" })
 public class ChallengeApplication {
 
   public static void main(String[] args) {
@@ -11,3 +11,4 @@ public class ChallengeApplication {
   }
 
 }
+
