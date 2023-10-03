eda6a8b67b7d63eea9e330065517b2da98eac227
Daniel Mendes <danielmendes.sw@gmail.com>
2023-10-04 01:23:22 +0200

    Feat: add tests for consult controller

---
+++ src/test/java/com/challenge/challenge/controller/ConsultControllerTest.java
@@ -0,0 +1,46 @@
+package com.challenge.challenge.controller;
+
+import com.challenge.challenge.controller.ConsultController;
+import com.challenge.challenge.service.ConsultService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.jupiter.api.Assertions.fail;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
+public class ConsultControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private ConsultService consultService;
+
+    @Autowired
+    private ConsultController consultController;
+
+    @BeforeEach
+    public void setUp() {
+        // Initialization if needed
+    }
+
+    @Test
+    public void testGetAllConsults() throws Exception {
+        // Performing the test request and asserting results
+        mockMvc.perform(get("/api/consults"))
+                .andExpect(status().isOk());
+    }
+
+
+}
