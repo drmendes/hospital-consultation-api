76cd0599639b1167a1e352c76989171fa7c3d605
Daniel Mendes <danielmendes.sw@gmail.com>
2023-10-04 01:24:14 +0200

    Feat: add tests for doctor controller

---
+++ src/test/java/com/challenge/challenge/controller/DoctorControllerTest.java
@@ -0,0 +1,37 @@
+package com.challenge.challenge.controller;
+
+import com.challenge.challenge.controller.DoctorController;
+import com.challenge.challenge.service.DoctorService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
+public class DoctorControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @BeforeEach
+    public void setUp() {
+        // Initialization if needed
+    }
+
+    @Test
+    public void testGetAllDoctors() throws Exception {
+        // Performing the test request and asserting results
+        mockMvc.perform(get("/api/doctors"))
+                .andExpect(status().isOk());
+
+    }
+}
